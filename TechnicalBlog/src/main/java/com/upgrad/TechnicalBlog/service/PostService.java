b2de7ebbc4c93ead980676ab16bcb5543c0250a0
Ramesh Gummadi <rameshgummadi@gmail.com>
2019-03-23 00:52:28 +0530

    msg

--- TechnicalBlog/src/main/java/com/upgrad/TechnicalBlog/service/PostService.java
+++ TechnicalBlog/src/main/java/com/upgrad/TechnicalBlog/service/PostService.java
@@ -1,5 +1,10 @@
 package com.upgrad.TechnicalBlog.service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -16,7 +21,9 @@ public class PostService {
 	public ArrayList<Post> getAllPosts() {
 		
 		ArrayList<Post> posts = new ArrayList<>();
-		Post post1 = new Post();
+		
+		
+		/*Post post1 = new Post();
 		post1.setTitle("Psot1");
 		post1.setBody("postBody 1");
 		post1.setDate(new Date());
@@ -33,7 +40,34 @@ public class PostService {
 		
 		posts.add(post1);
 		posts.add(post2);
-		posts.add(post3);
+		posts.add(post3);*/
+		Connection connection = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			
+			connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/technicalblog","postgres", "");
+			Statement statement = connection.createStatement();
+			ResultSet rs = statement.executeQuery("SELECT * FROM posts");
+			while(rs.next()) {
+				Post post = new Post();
+				post.setTitle(rs.getString("title"));
+				post.setBody(rs.getString("body"));
+				posts.add(post);
+				
+			}
+			
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally{
+			   try{
+			       connection.close();
+			       }catch(SQLException e){
+			      e.printStackTrace();
+			       }
+			       }
 		
 		return posts;
 		
@@ -41,12 +75,39 @@ public class PostService {
 	
 	public ArrayList<Post> getOnePost() {
 		ArrayList<Post> posts = new ArrayList<>();
-		Post post1 = new Post();
+		
+		
+		/*Post post1 = new Post();
 		post1.setTitle("This is your first post");
 		post1.setBody("This is your first post");
-		post1.setDate(new Date());
+		post1.setDate(new Date());*/
+		Connection connection = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			
+			connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/technicalblog","postgres", "");
+			Statement statement = connection.createStatement();
+			ResultSet rs = statement.executeQuery("SELECT * FROM posts where id = 4");
+			while(rs.next()) {
+				Post post = new Post();
+				post.setTitle(rs.getString("title"));
+				post.setBody(rs.getString("body"));
+				posts.add(post);
+				
+			}
+			
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		finally{
+			   try{
+			       connection.close();
+			       }catch(SQLException e){
+			      e.printStackTrace();
+			       }
+			       }
 		
-		posts.add(post1);
 		return posts;
 	}
 	
