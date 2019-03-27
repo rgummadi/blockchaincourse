f9be895b7ba53c8117ab5f15b0e9f0ad61430501
rgummadi <rgummadi@test.com>
2019-03-27 22:36:17 +0530

    msg

--- ImageHoster/src/main/java/com/upgrad/ImageHoster/Contoller/HomeController.java
+++ ImageHoster/src/main/java/com/upgrad/ImageHoster/Contoller/HomeController.java
@@ -1,14 +1,26 @@
 package com.upgrad.ImageHoster.Contoller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.upgrad.ImageHoster.model.Image;
+import com.upgrad.ImageHoster.service.ImageService;
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	private ImageService imageService;
+	
 	@RequestMapping("/")
-	public String getAllImages() {
+	public String getAllImages(Model model) {
 		
+		List<Image> images = imageService.getAllImages();
+		model.addAttribute("images",images);
 		return "index";
 	}
 }
